1eb9f013df0e68c975b95c151d666c8d434208d5
James Chu <James.Chu@aver.com>
2020-11-05 13:24:57 +0800

    First push

---
+++ chaos/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace chaos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox_num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_z0 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(66, 97);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 22);
+            this.textBox_a.TabIndex = 1;
+            this.textBox_a.Text = "3.45";
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(66, 136);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(100, 22);
+            this.textBox_n.TabIndex = 2;
+            this.textBox_n.Text = "10000";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(222, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(280, 276);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox_num
+            // 
+            this.textBox_num.Location = new System.Drawing.Point(66, 174);
+            this.textBox_num.Name = "textBox_num";
+            this.textBox_num.Size = new System.Drawing.Size(100, 22);
+            this.textBox_num.TabIndex = 4;
+            this.textBox_num.Text = "16";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "num";
+            // 
+            // textBox_z0
+            // 
+            this.textBox_z0.Location = new System.Drawing.Point(66, 211);
+            this.textBox_z0.Name = "textBox_z0";
+            this.textBox_z0.Size = new System.Drawing.Size(100, 22);
+            this.textBox_z0.TabIndex = 8;
+            this.textBox_z0.Text = "0.5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "z0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_z0);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_num);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox_num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_z0;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ chaos/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chaos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double funX(double x)
+        {
+            double a = double.Parse(textBox_a.Text);
+            return (a * x - a * x * x);
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox_n.Text);
+            int num = int.Parse(textBox_num.Text);
+            double z0 = double.Parse(textBox_z0.Text);
+            double x;
+
+            x = z0;
+
+            richTextBox1.Clear();
+
+            for (int i = 0; i < n; i++)
+                x = funX(x);
+            for (int i = 0; i< num; i++)
+            {
+                richTextBox1.AppendText(x.ToString() + "\n");
+                x = funX(x);
+            }
+
+        }
+    }
+}
